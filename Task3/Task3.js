ae8f072f57a8ab9fa71b11c6c65c719d3673e57a
MkNaira <71075949+MkNaira@users.noreply.github.com>
2020-09-20 04:01:40 +0400

    Update Task3.js

--- Task3/Task3.js
+++ Task3/Task3.js
@@ -1,3 +1,38 @@
-var a =5;
-var b = 8;
-alert a+b;
+ let code = parsefloat(prompt("enter the word with min lenght of 5 symbols"));
+if (code.length < = 5) {
+    alert("try again");
+
+} else {
+
+    alert ("code");
+}
+
+
+switch (code) {
+
+case "a";
+code = code + 4 ;
+break;
+case "e";
+code = code + 4 ;
+break;
+case "u";
+code = code + 4 ;
+break;
+case "i";
+code = code + 4 ;
+break;
+case "o";
+code = code + 4 ;
+break;
+case "z";
+code = code + 5 ;
+break;
+default;
+code = code - 1;
+
+}
+
+alert("code");
+
+
