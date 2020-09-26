59f3fa136b2600f911e040db9bb4fb69c18de2ed
MkNaira <71075949+MkNaira@users.noreply.github.com>
2020-09-27 02:04:47 +0400

    Create Task4.js

---
+++ 4/Task4.js
@@ -0,0 +1,21 @@
+let text1 = prompt("enter the word");
+let number1 = text1.length;
+result = "";
+for ( i = 0; i < = number1; i ++ ) {
+    result = result + text1.charAt(nummber - i);
+    }
+    alert("result");
+
+
+
+    let number2 = parseInt(prompt("enter number 2"));
+    let number3 = parseInt(prompt("enter number 3"));
+    let min_result = number2>number3?number3:number2;
+    let max_result = number2>number3?number2:number3;
+    alert(min_result);
+    alert(max_result);
+
+
+
+
+
